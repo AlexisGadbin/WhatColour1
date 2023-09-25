568af3d3cf1bbd38937c2e69678a7f3bbdbd0195
AlexisGadbin <alexis@gadbin.com>
2023-09-25 15:30:39 +0200

    Initial commit

---
+++ app/src/main/java/fr/eseo/e5e/ag/whatcolour/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.eseo.e5e.ag.whatcolour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
